28cd4d9edc3831d62cb86199a8aba4d7732c4eb1
Daish00 <jalezama2003@gmail.com>
2023-06-20 14:33:31 -0500

    gun roatation enemies
    las armas dan vueltitas :D

--- Assets/Game/Scripts/LevelScripts/RotationWeaponEnemy.cs
+++ Assets/Game/Scripts/LevelScripts/RotationWeaponEnemy.cs
@@ -4,22 +4,53 @@ using UnityEngine;
 
 public class RotationWeaponEnemy : MonoBehaviour
 {
-    public Transform jugador;
+    public Transform puntoRotacion;
+    public string jugadorTag = "Player";
+
+    private Transform jugador;
+
+    void Start()
+    {
+        // Buscar el objeto del jugador por su tag
+        GameObject jugadorObject = GameObject.FindWithTag(jugadorTag);
+        if (jugadorObject != null)
+        {
+            jugador = jugadorObject.transform;
+        }
+        else
+        {
+            Debug.LogWarning("No se encontró ningún objeto con la etiqueta " + jugadorTag);
+        }
+    }
 
     void Update()
     {
-        // Obtener la dirección hacia el jugador
-        Vector3 direccion = jugador.position - transform.position;
+        // Verificar si el objeto del jugador ha sido encontrado
+        if (jugador != null)
+        {
+            // Obtener la dirección hacia el jugador desde el punto de rotación
+            Vector3 direccion = jugador.position - puntoRotacion.position;
 
-        // Obtener el ángulo hacia el jugador en radianes
-        float anguloRad = Mathf.Atan2(direccion.y, direccion.x);
+            direccion.z = 0f;
 
-        // Convertir el ángulo a grados
-        float anguloDeg = anguloRad * Mathf.Rad2Deg;
+            // Obtener el ángulo hacia el jugador en radianes
+            float anguloRad = Mathf.Atan2(direccion.y, direccion.x);
 
-        // Rotar el objeto hacia el jugador
-        transform.rotation = Quaternion.Euler(0f, 0f, anguloDeg);
+            // Convertir el ángulo a grados
+            float anguloDeg = anguloRad * Mathf.Rad2Deg;
+
+            // Aplicar la rotación al arma en relación al punto de rotación
+            transform.rotation = Quaternion.Euler(0f, 0f, anguloDeg);
+
+            if (jugador.position.x > transform.position.x)
+            {
+                transform.localScale = new Vector3(1f, 1f, 1f);
+            }
+            else if (jugador.position.x < transform.position.x)
+            {
+                transform.localScale = new Vector3(1f, -1f, 1f);
+            }
+        }
     }
-
 
 }
