7cb788c17896a416016b698bea86b1f815c57f60
Teyliu <teyliu888@gmail.com>
2023-07-05 20:11:13 -0500

    Yes now is better
    Ell yeah

--- Assets/Game/Scripts/PerspectiveScripts/PerspectiveScript.cs
+++ Assets/Game/Scripts/PerspectiveScripts/PerspectiveScript.cs
@@ -4,39 +4,56 @@ using UnityEngine.Tilemaps;
 public class PerspectiveScript : MonoBehaviour
 {
     public Renderer playerRenderer;
-    public Tilemap tilemap;
+    public Tilemap[] tilemaps;
     public int playerSortingOrder = 0;
     public string tilesSortingLayer = "Default";
     public int tilesSortingOrderBelow = -1;
     public int tilesSortingOrderAbove = 1;
+    public Vector2 detectionOffset = Vector2.zero;
 
     private void LateUpdate()
     {
-        Vector3 playerPosition = playerRenderer.transform.position;
-        Vector3Int playerCellPosition = tilemap.WorldToCell(playerPosition);
-        Vector3 playerCellCenter = tilemap.GetCellCenterWorld(playerCellPosition);
+        Vector3 playerPosition = playerRenderer.transform.position + (Vector3)detectionOffset;
+        int tileSortingOrder = playerSortingOrder;
+        bool foundTile = false;
 
-        // Calculate the position within the tile
-        Vector3 tilePosition = playerPosition - playerCellCenter;
-
-        TileBase tile = tilemap.GetTile(playerCellPosition);
-
-        if (tile != null)
+        foreach (Tilemap tilemap in tilemaps)
         {
-            TilemapRenderer tilemapRenderer = tilemap.GetComponent<TilemapRenderer>();
-            int tileSortingOrder;
+            Vector3Int playerCellPosition = tilemap.WorldToCell(playerPosition);
+            Vector3 playerCellCenter = tilemap.GetCellCenterWorld(playerCellPosition);
 
-            if (tilePosition.y > 0)
-                tileSortingOrder = playerSortingOrder + tilesSortingOrderAbove;
-            else
-                tileSortingOrder = playerSortingOrder + tilesSortingOrderBelow;
+            // Calculate the position within the tile
+            Vector3 tilePosition = playerPosition - playerCellCenter;
 
-            tilemapRenderer.sortingLayerName = tilesSortingLayer;
-            tilemapRenderer.sortingOrder = tileSortingOrder;
+            TileBase tile = tilemap.GetTile(playerCellPosition);
+
+            if (tile != null)
+            {
+                TilemapRenderer tilemapRenderer = tilemap.GetComponent<TilemapRenderer>();
+
+                if (tilePosition.y > 0)
+                    tileSortingOrder += tilesSortingOrderAbove;
+                else
+                    tileSortingOrder += tilesSortingOrderBelow;
+
+                tilemapRenderer.sortingLayerName = tilesSortingLayer;
+                tilemapRenderer.sortingOrder = tileSortingOrder;
+                foundTile = true;
+
+                // Manually invalidate the tilemap's cache for the updated tile
+                tilemap.RefreshTile(playerCellPosition);
+            }
+        }
+
+        if (!foundTile)
+        {
+            // If no tile is found, set the sorting order based on the playerSortingOrder
             playerRenderer.sortingOrder = playerSortingOrder;
-
-            // Manually invalidate the tilemap's cache for the updated tile
-            tilemap.RefreshTile(playerCellPosition);
+        }
+        else
+        {
+            // Set the sorting order of the player based on the calculated tileSortingOrder
+            playerRenderer.sortingOrder = tileSortingOrder;
         }
     }
 }
